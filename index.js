c20071bee34173e6d461384b3bbbed5a590c1752
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-20 17:11:22 -0700

    Added the basic color and shape making of an SVG file

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+function createSVG(logoName, logoNameColor, logoShape, logoShapeColor) {
+    if (logoShape === "Circle") {
+return `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="40" fill="${logoShapeColor}" />
+</svg>`
+    } else if (logoShape === "Triangle") {
+return `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+    <polygon points="50,10 90,90 10,90" fill="${logoShapeColor}" />
+</svg>`
+    } else if (logoShape === "Square") {
+return `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+    <rect width="50" height="50" fill="${logoShapeColor}" />
+</svg>`
+    }
+}
+
 inquirer
     .prompt([
         {
@@ -11,7 +27,7 @@ inquirer
         {
             type: "input",
             name: "nameColor",
-            message: "What color do you want for the logo name? (color keyword or hexadecimal number)"
+            message: "What color do you want for the logo name? (color keyword or hexadecimal number including the hashtag)"
         },
         {
             type: "list",
@@ -22,9 +38,13 @@ inquirer
         {
             type: "input",
             name: "shapeColor",
-            message: "What color do you want for shape? (color keyword or hexadecimal number)"
+            message: "What color do you want for shape? (color keyword or hexadecimal number including the hashtag)"
         }
     ])
     .then((data) => {
         console.log(data);
+        const svg = createSVG(data.name, data.nameColor, data.shape, data.shapeColor);
+        fs.writeFile("data.svg", svg, (err) =>
+            err ? console.log(err) : console.log("Success!")
+        );
     })
