53763a68046f40ad2fa489a13d9da666a37c3391
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-23 15:50:02 -0700

    added the comments on functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { LogoGenerator } = require("./lib/shape");
 
 (async () => {
+    // this is the object from inquirer, using await so that things happen in the right process
     const userInput = await LogoGenerator.promptUser();
-
+    // this is the user input stored in the individual pieces so that it can go into the constructor of the logoGenerator
     const logoGenerated = new LogoGenerator (
         userInput.name,
         userInput.nameColor,
